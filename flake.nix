4c099e243fd80355b4191b6a153b51e8fa4805bb
nic <christoph.niederer99@gmail.com>
2026-05-26 06:54:58 +0200

    test

--- flake.nix
+++ flake.nix
@@ -12,26 +12,23 @@
 		hyprland.url = "github:hyprwm/Hyprland";
 	};
 
-	outputs = {self, nixpkgs, home-manager, hyprland, ...  } @ inputs:
-		let mkSystem = hostname: system: extraModules:
-			nixpkgs.lib.nixosSystem {
-				inherit system;
+				# ./modules/common
+	outputs = {self, nixpkgs, home-manager, hyprland, ...  } @ inputs: {
+		nixosConfigurations = {
+			desktop = nixpkgs.lib.nixosSystem {
+				system = "x86_64-linux";
 				specialArgs = { inherit inputs; };
 				modules = [
-					./modules/common
-					home-manager.nixosModules.home-manager {
-						home-manager.useGlobalPkgs = true;
-						home-manager.useUserPackages = true;
-						home-manager.extraSpecialArgs = { inherit inputs; };
-						home-manager.users.nic = import ./home/common;
-					}
-				./hosts/${hostname}
-				] ++ extraModules;
-			};
-		in {
-			nixosConfigurations = {
-				desktop = mkSystem "desktop" "x86_64-linux" [];
+				./hosts/desktop
+				home-manager.nixosModules.home-manager {
+					home-manager.useGlobalPkgs = true;
+					home-manager.useUserPackages = true;
+					home-manager.extraSpecialArgs = { inherit inputs; };
+					home-manager.users.nic = import ./home/common;
+				}
+				];
 			};
 		};
+	};
 }
 
--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,7 +1,31 @@
-{...}:
+{ config, pkgs, ... }:
+
 {
-	imports = [
-		./configuration.nix
-		./hardware-configuration.nix
-	];
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "nixos";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  time.timeZone = "Europe/Vienna";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.nic = {
+    description = "Christoph";
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" ];
+    # shell = pkgs.zsh;
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  system.stateVersion = "25.11";
 }
