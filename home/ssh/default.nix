aed7737d73a13ebdfb2f1e2941302e702bd35608
nic <christoph.niederer99@gmail.com>
2026-06-11 18:41:46 +0200

    feat(add): add ssh keys

--- home/ssh/default.nix
+++ home/ssh/default.nix
@@ -1,6 +1,17 @@
 { config, pkgs, ... }:
 
 {
+  sops.secrets = {
+    "git_ed25519" = {
+      path = "${config.home.homeDirectory}/.ssh/git_ed25519";
+      mode = "0600";
+    };
+    "git_ed25519_pub" = {
+      path = "${config.home.homeDirectory}/.ssh/git_ed25519.pub";
+      mode = "0644";
+    };
+  };
+
   programs.ssh = {
     enable = true;
 
