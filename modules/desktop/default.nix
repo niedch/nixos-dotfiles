ae7a0dbe2992fc39a41da9139bf3a3c79a83d478
nic <christoph.niederer99@gmail.com>
2026-06-12 17:24:43 +0200

    feat: skip checking remote for firmware

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -1,10 +1,22 @@
-{...}:
+{ pkgs, lib, ... }:
 {
   imports = [
     ./hyprland.nix
     ./displaymanager.nix
     ./fonts.nix
   ];
+
+  services.fwupd = {
+    enable = true;
+    extraRemotes = [];
+  };
+
+  # fwupd-refresh.service periodically checks for firmware updates from LVFS.
+  # It runs as fwupd-refresh user which can't read secret.key needed for
+  # the embargo remote (pre-release firmware under NDA, requires vendor auth).
+  # Skip embargo since we don't use it.
+  systemd.services.fwupd-refresh.serviceConfig.ExecStart =
+    lib.mkForce "${pkgs.fwupd}/bin/fwupdmgr refresh --ignore-embargo";
 
   # localsend uses this port for LAN discovery and file transfer
   networking.firewall = {
