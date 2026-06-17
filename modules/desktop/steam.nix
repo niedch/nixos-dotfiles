d8b979b8d997d2b354d9fb88e51de6b61538788c
nic <christoph.niederer99@gmail.com>
2026-06-17 10:24:38 +0200

    generation 207: feat(steam): Add NVIDIA Prime offload support

--- modules/desktop/steam.nix
+++ modules/desktop/steam.nix
@@ -1,8 +1,21 @@
-{pkgs, ...}: {
+{config, pkgs, lib, ...}: {
   programs.steam = {
     enable = true;
     extraCompatPackages = [
       pkgs.proton-ge-bin
     ];
   };
+
+  environment.systemPackages = lib.mkIf config.hardware.nvidia.prime.offload.enable [
+    (let
+      patchDesktop = pkg: appName: from: to: lib.hiPrio (
+        pkgs.runCommand "patched-desktop-entry-for-${appName}" {} ''
+          ${pkgs.coreutils}/bin/mkdir -p $out/share/applications
+          ${pkgs.gnused}/bin/sed 's#${from}#${to}#g' \
+            < ${pkg}/share/applications/${appName}.desktop \
+            > $out/share/applications/${appName}.desktop
+        '');
+    in
+      patchDesktop pkgs.steam "steam" "^Exec=" "Exec=nvidia-offload ")
+  ];
 }
