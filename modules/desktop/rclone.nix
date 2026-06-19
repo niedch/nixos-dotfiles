63ea25f0605ec0f5e9cb6bcb7f60f63435a92674
nic <christoph.niederer99@gmail.com>
2026-06-19 07:35:07 +0200

    generation 260: feat(rclone): Add support for multiple mounts and Icloud remote

--- modules/desktop/rclone.nix
+++ modules/desktop/rclone.nix
@@ -1,22 +1,35 @@
 {config, pkgs, ...}: let
-  remote = "G-Drive:Documents";
-  mountPoint = "/media/Google-Drive";
+  mounts = [
+    {
+      name = "G-Drive";
+      remote = "G-Drive:Documents";
+      mountPoint = "/media/Google-Drive";
+    }
+    {
+      name = "I-Cloud";
+      remote = "Icloud:";
+      mountPoint = "/media/Icloud";
+    }
+  ];
 in {
   environment.systemPackages = [pkgs.rclone];
 
-  systemd.services.rclone-mount = {
-    description = "rclone mount service (G-Drive)";
-    after = ["network-online.target"];
-    wants = ["network-online.target"];
-    wantedBy = ["multi-user.target"];
+  systemd.services = builtins.listToAttrs (map (mount: {
+    name = "rclone-mount-${mount.name}";
+    value = {
+      description = "rclone mount service (${mount.name})";
+      after = ["network-online.target"];
+      wants = ["network-online.target"];
+      wantedBy = ["multi-user.target"];
 
-    serviceConfig = {
-      Type = "simple";
-      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${mountPoint}";
-      ExecStart = "${pkgs.rclone}/bin/rclone mount ${remote} ${mountPoint} --config ${config.sops.secrets.rclone-config.path} --allow-other --vfs-cache-mode writes --dir-perms 0755 --file-perms 0644";
-      ExecStop = "${pkgs.fuse3}/bin/fusermount3 -u ${mountPoint}";
-      Restart = "on-failure";
-      RestartSec = "10s";
+      serviceConfig = {
+        Type = "simple";
+        ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${mount.mountPoint}";
+        ExecStart = "${pkgs.rclone}/bin/rclone mount ${mount.remote} ${mount.mountPoint} --config ${config.sops.secrets.rclone-config.path} --allow-other --vfs-cache-mode writes --dir-perms 0755 --file-perms 0644";
+        ExecStop = "${pkgs.fuse3}/bin/fusermount3 -u ${mount.mountPoint}";
+        Restart = "on-failure";
+        RestartSec = "10s";
+      };
     };
-  };
+  }) mounts);
 }
