f8a265f8be0a566de77b078622720cbbec88675f
nic <christoph.niederer99@gmail.com>
2026-06-22 07:20:53 +0200

    generation 290: chore: Update nix-omarchy-theme and remove dynamic GTK theme setup

--- home/hyprland/conf/env.lua
+++ home/hyprland/conf/env.lua
@@ -2,6 +2,7 @@ hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Classic")
 hl.env("HYPRCURSOR_SIZE", "24")
+
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("GDK_BACKEND", "wayland,x11,*")
 hl.env("MOZ_ENABLE_WAYLAND", "1")
@@ -11,23 +12,4 @@ hl.env("XDG_SESSION_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_TYPE", "wayland")
 hl.env("WLR_NO_HARDWARE_CURSORS", "1")
 hl.env("WLR_DRM_DEVICES", "/dev/dri/by-path/pci-0000:00:02.0-card")
-
-local gtk_theme_file = os.getenv("HOME") .. "/.themes-src/current/gtk.theme"
-local f = io.open(gtk_theme_file, "r")
-if f then
-  local gtk_theme = f:read("*l")
-  f:close()
-  local light_file = io.open(os.getenv("HOME") .. "/.themes-src/current/light.mode", "r")
-  if light_file then
-    light_file:close()
-    hl.env("GTK_THEME", gtk_theme)
-  else
-    if not gtk_theme:match("%-dark$") then
-      gtk_theme = gtk_theme .. ":dark"
-    end
-    hl.env("GTK_THEME", gtk_theme)
-  end
-else
-  hl.env("GTK_THEME", "Adwaita:dark")
-end
 
