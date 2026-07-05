9b16213a1c8762ae01290f1ade42924a3d8cb709
nic <christoph.niederer99@gmail.com>
2026-07-05 13:55:38 +0200

    hyprland follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -9,7 +9,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     nix-omarchy-theme = {
       url = "github:niedch/nix-omarchy-theme";
