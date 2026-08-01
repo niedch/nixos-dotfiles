594de586f7035f541db4d672fda5773b4b2b14ab
nic <christoph.niederer99@gmail.com>
2026-08-01 09:46:45 +0200

    refactor: Cpu util

--- home/quickshell/config/scripts/cpu.sh
+++ home/quickshell/config/scripts/cpu.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
+set -u
 
-p1=$(awk '/^cpu / {t=$2+$3+$4+$5+$6+$7+$8+$9+$10+$11; i=$5+$6; print t, i}' /proc/stat)
+cpu_sample() {
+    awk '
+        /^cpu / {
+            for (i = 2; i <= NF; i++) total += $i
+            idle = $5 + $6
+            print total, idle
+        }
+    ' /proc/stat
+}
+
+read -r t1 i1 <<< "$(cpu_sample)"
 sleep 0.2
-p2=$(awk '/^cpu / {t=$2+$3+$4+$5+$6+$7+$8+$9+$10+$11; i=$5+$6; print t, i}' /proc/stat)
+read -r t2 i2 <<< "$(cpu_sample)"
 
-read -r t1 i1 <<<"$p1"
-read -r t2 i2 <<<"$p2"
-
-usage=$(awk -v t1="$t1" -v i1="$i1" -v t2="$t2" -v i2="$i2" 'BEGIN { dt=t2-t1; di=i2-i1; if (dt>0) printf "%.1f", 100*(dt-di)/dt; else printf "0" }')
+usage=$(awk -v t1="$t1" -v i1="$i1" -v t2="$t2" -v i2="$i2" '
+    BEGIN {
+        dt = t2 - t1
+        di = i2 - i1
+        printf "%.1f", (dt > 0) ? 100 * (dt - di) / dt : 0
+    }
+')
 
 read -r l1 l2 l3 _ < /proc/loadavg
 
-echo "$usage|$l1|$l2|$l3"
+printf "%s|%s|%s|%s\n" "$usage" "$l1" "$l2" "$l3"
