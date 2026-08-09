6665b7245764604324dbbf04b23e8d818708c443
nic <christoph.niederer99@gmail.com>
2026-08-09 12:36:40 +0200

    feat: Add calls

--- modules/common/cache-config.nix
+++ modules/common/cache-config.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   nix.settings = lib.mkIf (config.networking.hostName != "dobby") {
-    substituters = ["http://dobby:5000"];
+    substituters = ["http://dobby:5000" "https://cache.nixos.org"];
     trusted-public-keys = ["dobby:rrZQzoRX5Glj/0fX+bFGJ6YUPoxb3z/hg5K84k2G8yo="];
   };
 }
--- modules/common/nix-gc.nix
+++ modules/common/nix-gc.nix
@@ -1,7 +1,33 @@
-{...}: {
+{pkgs, ...}: let
+  gcStarted = pkgs.writeShellScript "nix-gc-started" ''
+    /run/current-system/sw/bin/post-homepage-message-board "Nix garbage collection started" info
+  '';
+  gcDone = pkgs.writeShellScript "nix-gc-done" ''
+    /run/current-system/sw/bin/post-homepage-message-board "Nix garbage collection completed" success
+  '';
+  gcFailed = pkgs.writeShellScript "nix-gc-failed" ''
+    /run/current-system/sw/bin/post-homepage-message-board "Nix garbage collection failed" error
+  '';
+in {
   nix.gc = {
     automatic = true;
     dates = "daily";
     options = "--delete-older-than 5d";
+  };
+
+  systemd.services.nix-gc = {
+    onFailure = ["nix-gc-failure.service"];
+    serviceConfig = {
+      ExecStartPre = [gcStarted];
+      ExecStartPost = [gcDone];
+    };
+  };
+
+  systemd.services.nix-gc-failure = {
+    description = "Post nix GC failure to message board";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = gcFailed;
+    };
   };
 }
