07fc6da9136a50bea2fd2d1aa72c1ac53d9f25e1
nic <christoph.niederer99@gmail.com>
2026-08-14 06:51:03 +0200

    fix: Plugin handling

--- home/quickshell-omarchy/default.nix
+++ home/quickshell-omarchy/default.nix
@@ -28,11 +28,27 @@
             {id = "omarchy.tray";}
             {id = "omarchy.audio";}
             {id = "omarchy.network";}
+            {id = "saif.system-stats";}
+            {
+              id = "b.omastonk";
+              symbol = "BTC-USD";
+            }
             {id = "omarchy.power";}
           ];
         };
       };
       plugins = [];
+    };
+
+    plugins = {
+      "saif.system-stats" = {
+        git = "https://github.com/SaifOmar/SystemStats";
+        rev = "8ea500546d58739c36520899d51ae30276859cd0";
+      };
+      "b.omastonk" = {
+        git = "https://github.com/brianblakely/omastonk";
+        rev = "76a29b16dfc17251953b31df2c0a7cd2ab381e6c";
+      };
     };
   };
 }
